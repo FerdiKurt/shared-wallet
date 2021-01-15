583e0965a155f853f09cb6e1c89fe04e7aba1b0c
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-15 18:39:31 +0300

    events and modifiers added.

---
+++ contracts/SharedWalletAbstract.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.8.0;
+
+import './Storage.sol';
+
+abstract contract SharedWalletAbstract is Storage{
+    event AllowanceChanged(
+        address indexed _from, 
+        address indexed _to, 
+        uint _oldAmount,
+        uint _newAmount
+    );
+    event EtherWithdraw(address indexed _to, uint _amount);
+    event ContractReceivedEther(address indexed _addr, uint _amount);
+    
+    modifier availableAmount(uint _amount) {
+        require(allowances[msg.sender] >= _amount, 'Not enough Ether!');
+        _;
+    }
+    
+    modifier onlyOwner() {
+        require(owners[msg.sender] == true, 'Only owner!');   
+        _;
+    }
+
+    modifier addressZero(address _addr) {
+        require(_addr != address(0x00), 'recipient cannot be zero!');
+        _;
+    }   
+}
