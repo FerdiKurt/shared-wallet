6a4956123812bc2a27b8f25ea47cf3510ac60644
FerdiKurt <36864124+FerdiKurt@users.noreply.github.com>
2021-01-23 01:45:05 +0300

    Update SharedWalletAbstract.sol

--- contracts/SharedWalletAbstract.sol
+++ contracts/SharedWalletAbstract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0 <0.8.0;
 
 import './Storage.sol';
 
-abstract contract SharedWalletAbstract is Storage{
+abstract contract SharedWalletAbstract is Storage {
     event AllowanceChanged(
         address indexed _from, 
         address indexed _to, 
